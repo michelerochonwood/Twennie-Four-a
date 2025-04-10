d0d6c638457110f0edd92ea1ba1a20fe4aeaccbd
michelerochonwood <michelelrochon@gmail.com>
2025-04-10 13:52:58 -0400

    apr10withstripe

--- routes/memberroutes/index.js
+++ routes/memberroutes/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const memberController = require('../../controllers/memberController');
+const baseUrl = process.env.BASE_URL || 'http://localhost:3000';
 
 // Member Routes
 router.get('/form', memberController.showMemberForm);
@@ -21,6 +22,44 @@ router.get('/choose', (req, res) => {
         title: 'Registration Successful',
         username
     });
+});
+
+router.get('/payment/success', (req, res) => {
+  res.send('Payment successful! You’ll be registered shortly.');
+});
+
+router.get('/payment/cancel', (req, res) => {
+  res.send('Payment canceled. You can try again anytime.');
+})
+
+
+router.post('/payment', async (req, res) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'payment',
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Twennie Paid Individual Membership',
+            },
+            unit_amount: 1500, // $15.00 (in cents)
+          },
+          quantity: 1,
+        },
+      ],
+      success_url: `${baseUrl}/member/payment/success`,
+      cancel_url: `${baseUrl}/member/payment/cancel`,
+      
+    });
+
+    res.redirect(303, session.url); // redirects to Stripe checkout
+  } catch (err) {
+    console.error('Stripe payment error:', err);
+    res.status(500).send('An error occurred with Stripe');
+  }
 });
 
 module.exports = router;
