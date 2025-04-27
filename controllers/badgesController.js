18ae37560abbac3e4c617b4fe95003e02b64812b
michelerochonwood <michelelrochon@gmail.com>
2025-04-27 11:23:50 -0400

    badgesagain

--- controllers/badgesController.js
+++ controllers/badgesController.js
@@ -1,3 +1,7 @@
+exports.showBadgesView = (req, res) => {
+  res.render('badges_view', { layout: 'badgeslayout' });
+};
+
 exports.pickBadge = (req, res) => {
   console.log('✅ Received POST to /badges/pick');
   console.log('Request body:', req.body);
@@ -9,12 +13,12 @@ exports.pickBadge = (req, res) => {
     return res.status(400).json({ error: 'Badge path and badge name are required.' });
   }
 
-  // Save badge selection to session
   req.session.selectedBadge = { image: badgePath, name: badgeName };
   console.log('✅ Badge saved to session:', req.session.selectedBadge);
 
   res.json({ success: true, selectedBadge: req.session.selectedBadge });
 };
+
   
   
   
